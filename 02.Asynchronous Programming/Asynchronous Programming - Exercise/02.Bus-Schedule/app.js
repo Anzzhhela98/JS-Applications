a18bf07616f19c5c28c9da4757a682395fabf6d9
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-07-03 20:39:30 +0300

    Asynchronous programing

--- 02.Asynchronous Programming/Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ 02.Asynchronous Programming/Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,29 @@
 function solve() {
+    const departBTN = document.getElementById('depart');
+    const arriveBTN = document.getElementById('arrive');
+    const banner = document.querySelector('#info>span');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stop = {
+        next: 'depot'
+    }
+
+    async function depart() {
+        const url = 'http://localhost:3030/jsonstore/bus/schedule/' + stop.next;
+        const respone = await fetch(url);
+        const data = await respone.json();
+
+        stop = data;
+        banner.textContent = `Next stop ${stop.name}`;
+
+        departBTN.disabled = true;
+        arriveBTN.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        banner.textContent = `Arriving at ${stop.name}`;
+
+        departBTN.disabled = false;
+        arriveBTN.disabled = true;
     }
 
     return {
