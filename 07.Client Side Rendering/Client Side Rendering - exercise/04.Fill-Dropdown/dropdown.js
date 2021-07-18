347935d5b2b4d02be1e749980bc032b7136cb24d
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-07-18 18:29:22 +0300

    Homework solved with lit-html

--- 07.Client Side Rendering/Client Side Rendering - exercise/04.Fill-Dropdown/dropdown.js
+++ 07.Client Side Rendering/Client Side Rendering - exercise/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,50 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+const container = document.querySelector('div');
+const input = document.getElementById('itemText');
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown';
+
+getData();
+
+async function getData() {
+    document.querySelector('form').addEventListener('submit', (e) => addItem(event, list))
+    const response = await fetch(url);
+
+    if (response.ok == false) {
+        const error = await response.json();
+        return alert(error.message);
+    }
+
+    const data = await response.json();
+    let list = Object.values(data);
+
+    update(list)
+}
+
+async function addItem(event, list) {
+    event.preventDefault();
+    if (input.value) {
+        const item = input.value
+
+        const response = await fetch(url, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ text: item }),
+        });
+
+        const result = await response.json();
+        list.push(result);
+        input.value = ' ';
+        update(list);
+    }
+}
+
+const createTemplate = (list) =>
+    html `<select id="menu">
+    ${list.map((x) => html`<option value="${x._id}">${x.text}
+    </option>`)};
+</select>`;
+
+function update(list) {
+    const result = createTemplate(list);
+    render(result, container);
 }
--- 07.Client Side Rendering/Client Side Rendering - exercise/05.Table-Search/solution.js
+++ 07.Client Side Rendering/Client Side Rendering - exercise/05.Table-Search/solution.js
@@ -1,8 +1,31 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { html, render } from "./node_modules/lit-html//lit-html.js"
+const container = document.querySelector('tbody');
+const url = 'http://localhost:3030/jsonstore/advanced/table';
+const input = document.getElementById('searchField');
 
-   function onClick() {
-      //   TODO:
+const createTemplate = (student, match) =>
+    html `<tr class="${match == true ? 'select' : ''}">
+   <td>${student.firstName} ${student.lastName}}</td>
+   <td>${student.email}</td>
+   <td>${student.course}</td>
+</tr>`;
 
-   }
+async function initialize() {
+    input.addEventListener('input', () => update(list, input.value));
+
+    const studentsData = await (await fetch(url)).json();
+    const list = Object.values(studentsData);
+
+    update(list);
+}
+
+initialize()
+
+function update(list, match = '') {
+    const result = list.map(x => createTemplate(x, compare(x, match)));
+    render(result, container);
+}
+
+function compare(item, match) {
+    return Object.values(item).some((v) => match && v.toLowerCase().includes(match.toLowerCase()));
 }
